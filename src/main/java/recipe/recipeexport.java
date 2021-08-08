dd406621894f50e34ce5606674f9904b52b3b11c
mayongxin <mayongxin@outlook.com>
2021-08-08 22:49:07 +0800

    idea test commit

---
+++ src/main/java/recipe/recipeexport.java
@@ -0,0 +1,9 @@
+package recipe;
+
+public class recipeexport {
+    public static void main(String[] args){
+        System.out.println("北京你好！");
+        System.out.println("世界你好！");
+        System.out.println("多挣钱！");
+    }
+}
