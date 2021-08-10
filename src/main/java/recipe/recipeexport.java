195d107c442ea9d3a429f286276596b8cfd63a91
mayongxin <mayongxin@outlook.com>
2021-08-10 21:39:16 +0800

    machine test commit4

--- src/main/java/recipe/recipeexport.java
+++ src/main/java/recipe/recipeexport.java
@@ -2,15 +2,6 @@ package recipe;
 
 public class recipeexport {
     public static void main(String[] args){
-        System.out.println("北京你好！");
-        System.out.println("世界你好！");
-        System.out.println("多挣钱！");
-        System.out.println("我好难");
-        System.out.println("太难了！");
-        System.out.println("git1！");
-        System.out.println("git2！");
-        System.out.println("git3！");
-        System.out.println("git4！");
-        System.out.println("git5！");
+        System.out.println("老马加油！！");
     }
 }
