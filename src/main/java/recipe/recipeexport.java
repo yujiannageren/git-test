e0db1f8ec5f499cf3c3cf1d5b5fe2ef8852501ff
mayongxin <mayongxin@outlook.com>
2021-08-10 21:08:45 +0800

    second test commit

--- src/main/java/recipe/recipeexport.java
+++ src/main/java/recipe/recipeexport.java
@@ -5,5 +5,7 @@ public class recipeexport {
         System.out.println("北京你好！");
         System.out.println("世界你好！");
         System.out.println("多挣钱！");
+        System.out.println("我好难");
+        System.out.println("太难了！");
     }
 }
